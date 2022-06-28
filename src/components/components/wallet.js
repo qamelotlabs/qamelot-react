097166ce4d41caeac62089a9588fcc76f84fc9fb
Sagar-codes <paulsagar5433@gmail.com>
2022-06-28 15:43:48 +0530

    added metamask

--- src/components/components/wallet.js
+++ src/components/components/wallet.js
@@ -1,17 +1,71 @@
-import React from 'react';
+import {React, useState, useEffect } from 'react';
 import { createGlobalStyle } from 'styled-components';
+import { ethers } from 'ethers';
 
-const Wallet= () => (
-  <div className="row">
-    <div className="col-lg-3 mb30 metamask">
-        <span className="box-url">
-            <span className="box-url-label">Most Popular</span>
-            <img src="./img/wallet/1.png" alt="" className="mb20"/>
-            <h4>Metamask</h4>
-            <p>Start exploring blockchain applications in seconds.  Trusted by over 1 million users worldwide.</p>
-            <button type="button" className="btn btn-primary">Connect</button>
-        </span>
-    </div>
+const Wallet= () => {
+    const [isConnected, setIsConnected] = useState(false);
+
+    const [accountAddress, setAccountAddress] = useState('');
+
+    const [haveMetamask, sethaveMetamask] = useState(true);
+
+    const [accountBalance, setAccountBalance] = useState(true);
+
+    const { ethereum } = window;
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    useEffect(() => {
+        const checkMetamaskAvailability = async () => {
+          if (!ethereum) {
+            sethaveMetamask(false);
+          }
+          sethaveMetamask(true);
+        };
+        checkMetamaskAvailability();
+      }, []);
+
+      const connectWallet = async () => {
+            try {
+                if (!ethereum) {
+                    sethaveMetamask(false);
+                }
+
+                const accounts = await ethereum.request({
+                    method: 'eth_requestAccounts',
+                });
+
+                let balance = await provider.getBalance(accounts[0]);
+                let bal = ethers.utils.formatEther(balance);
+
+                setAccountAddress(accounts[0]);
+                setAccountBalance(bal);
+                setIsConnected(true);
+            } catch (error) {
+                setIsConnected(false);
+            }
+        };
+    return (
+        <div className="row">
+            <div className="col-lg-3 mb30 metamask">
+                <span className="box-url">
+                    <span className="box-url-label">Most Popular</span>
+                    <img src="./img/wallet/1.png" alt="" className="mb20"/>
+                    <h4>Metamask</h4>
+                    <p>Start exploring blockchain applications in seconds.  Trusted by over 1 million users worldwide.</p>
+                    <button type="button" onClick={connectWallet} className="btn btn-primary" >Connect</button>
+                </span>
+            </div>
+            <div className="col-lg-3 mb30">
+                <h1 className='address'>address:   <p>
+                    {accountAddress}
+                  </p>
+                </h1>
+                <br></br>
+                <h1>
+                    Balance :    <p>{accountBalance}</p>
+                </h1>
+            </div>
 
     {/* <div className="col-lg-3 mb30">
         <span className="box-url">
@@ -72,4 +126,7 @@ const Wallet= () => (
     </div>                                   */}
 </div>
 );
+}
+
+  
 export default Wallet;
